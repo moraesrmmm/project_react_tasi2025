a3791819c4529a05c1edae2d3e11e8d98fdf899e
Rômulo Moraes <romulo.moraes@spsp.com.br>
2025-05-08 19:41:40 -0300

    feat: template

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Registrar from './views/Registrar';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
+import Home from './views/public/Home';
 
 // Template
-import Template from './components/Template';
+import TemplateAdmin from './components/TemplateAdmin';
+import TemplatePublic from './components/TemplatePublic';
+import { lightBlue } from '@mui/material/colors';
 
 const darkTheme = createTheme({
   palette: {
@@ -26,27 +29,49 @@ const darkTheme = createTheme({
 const Middleware = () => {
   const isLogged = localStorage.getItem('logado') === 'true';
 
-  return isLogged ? (
-    <Template>
-      <Outlet />
-    </Template>
+  return isLogged? (
+    <Outlet />
   ) : (
     <Navigate to="/login" />
   );
 };
 
 const App = () => {
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#1976d2', // azul padrão do MUI
+      },
+      secondary: {
+        main: '#dc004e',
+      },
+      background: {
+        default: '#f5f5f5',
+        paper: '#fff',
+      },
+    },
+  });
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+          {/* ROTAS PÚBLICAS - usam o TemplatePublic */}
           <Route path="/login" element={<Login />} />
           <Route path="/registrar" element={<Registrar />} />
+          <Route element={<TemplatePublic />}>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/home" element={<Home />} />
+          </Route>
+
+          {/* ROTAS ADMIN - protegidas por login e perfil */}
           <Route element={<Middleware />}>
-            {/* Suas rotas protegidas vão aqui */}
-            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route element={<TemplateAdmin />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/views/public/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+          ddsa
+        </div>
+    );
+};
+
+export default Home;
